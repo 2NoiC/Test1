b7026be9704c5d6714d13fab2a04b6e8fb2fa9b0
2NoiC <dr_dreamer@list.ru>
2020-12-01 20:18:37 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,7 @@
 import requests
 
 domain = "https://api.vk.com/method"
-access_token = '644c6f1ee15c472b37b3a0f41bfeccc3fdd3d9ea3f371f46aa9e838a9062aeeb4c6b5d662faef4930ecbf'
+access_token = 'your token'
 user_id = '157844385'
 fields = 'sex'
 v = '5.124'
